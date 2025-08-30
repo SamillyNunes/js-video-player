7a68e85aac404660713c326fe1a82b4b3a6d73e2
Samilly Nunes <samillynunes19@gmail.com>
2025-08-30 10:41:13 -0300

    feat: added event listeners on viewer and play button

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const player = document.querySelector('.player');
 const viewer = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const play = player.querySelector('.play');
+const playButton = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
@@ -14,3 +14,5 @@ function togglePlay() {
 }
 
 // Adicioanr listeners
+viewer.addEventListener('click', togglePlay);
+playButton.addEventListener('click', togglePlay);
