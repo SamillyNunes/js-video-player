36d754e20087202ea5886a588eac09955d077abc
Samilly Nunes <samillynunes19@gmail.com>
2025-08-30 10:25:48 -0300

    js: query elements inside document

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Buscar os elementos
+const player = document.querySelector('.player');
+const viewer = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const play = player.querySelector('.play');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Construir as funcoes
+
+// Adicioanr listeners
